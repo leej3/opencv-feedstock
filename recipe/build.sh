dc473f6eeedc78c6835dba382de68cf3106e3153
leej3 <johnleenimh@gmail.com>
2021-12-17 05:26:10 -0500

    working except for libprotobuf run dep

--- recipe/build.sh
+++ recipe/build.sh
@@ -26,14 +26,43 @@ fi
 mkdir build
 cd build
 
-
-#-DWITH_VA_INTEL=OFF -DWITH_VA=OFF -DWITH_OPENCLAMDBLAS=OFF -DWITH_OPENCLAMDFFT=OFF -DWITH_GTK=OFF -DWITH_GSTREAMER=OFF -DWITH_1394=OFF -DWITH_JASPER=ON -GNinja ..
-#export PKG_CONFIG_LIBDIR=$PREFIX/lib
 #export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
+export PKG_CONFIG_LIBDIR=$PREFIX/lib
 export CMAKE_SYSTEM_PREFIX_PATH=${SYS_PREFIX}
   cmake .. -LAH                                                             \
     -GNinja                                                                 \
+    -DBUILD_DOCS=0                                                          \
+    -DBUILD_JASPER=0                                                        \
+    -DBUILD_JPEG=0                                                          \
+    -DBUILD_LIBPROTOBUF_FROM_SOURCES=0                                      \
+    -DBUILD_OPENEXR=ON                                                      \
+    -DBUILD_PERF_TESTS=0                                                    \
+    -DBUILD_PNG=0                                                           \
+    -DBUILD_PROTOBUF=0                                                      \
+    -DBUILD_TESTS=0                                                         \
+    -DBUILD_TIFF=0                                                          \
+    -DBUILD_ZLIB=0                                                          \
+    -DBUILD_opencv_apps=OFF `# issue linking with opencv_model_diagnostics` \
+    -DUPDATE_PROTO_FILES=ON                                                 \
+    -DWITH_1394=OFF                                                         \
+    -DWITH_CUDA=OFF                                                         \
+    -DWITH_EIGEN=1                                                          \
+    -DWITH_FFMPEG=ON                                                        \
+    -DWITH_GTK=OFF                                                          \
+    -DWITH_JASPER=OFF                                                       \
+    -DWITH_LAPACK=OFF                                                       \
+    -DWITH_MATLAB=OFF                                                       \
+    -DWITH_OPENCL=OFF                                                       \
+    -DWITH_OPENCLAMDBLAS=OFF                                                \
+    -DWITH_OPENCLAMDFFT=OFF                                                 \
+    -DWITH_OPENMP=1                                                         \
+    -DWITH_OPENNI=OFF                                                       \
+    -DWITH_TESSERACT=OFF                                                    \
+    -DWITH_VA=OFF                                                           \
+    -DWITH_VA_INTEL=OFF                                                     \
+    -DWITH_VTK=OFF                                                          \
     -DPYTHON_DEFAULT_EXECUTABLE=$(which python)                             \
+    -DCMAKE_CROSSCOMPILING=ON                                               \
     -DCMAKE_BUILD_TYPE="Release"                                            \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}                                        \
     -DCMAKE_INSTALL_LIBDIR=lib                                              \
@@ -41,42 +70,12 @@ export CMAKE_SYSTEM_PREFIX_PATH=${SYS_PREFIX}
     -DCMAKE_SKIP_RPATH=ON                                                   \
     -DCMAKE_STRIP="${STRIP}"                                                \
     -DOPENCV_DOWNLOAD_PATH="${SYS_PREFIX}"/conda-bld/src_cache              \
-    -DWITH_OPENMP=1                                                         \
-    -DBUILD_opencv_dnn=0                                                    \
-    -DWITH_EIGEN=1                                                          \
-    -DBUILD_TESTS=0                                                         \
-    -DBUILD_DOCS=0                                                          \
-    -DBUILD_PERF_TESTS=0                                                    \
-    -DBUILD_ZLIB=0                                                          \
-    -DBUILD_LIBPROTOBUF_FROM_SOURCES=0                                      \
-    -DBUILD_PROTOBUF=0                                                      \
-    -DBUILD_TIFF=0                                                          \
-    -DBUILD_PNG=0                                                           \
-    -DBUILD_OPENEXR=ON                                                      \
-    -DBUILD_JASPER=0                                                        \
-    -DBUILD_JPEG=0                                                          \
-    -DWITH_CUDA=OFF                                                         \
-    -DWITH_OPENCL=OFF                                                       \
-    -DWITH_OPENNI=OFF                                                       \
-    -DWITH_FFMPEG=ON                                                        \
-    -DWITH_MATLAB=OFF                                                       \
-    -DWITH_VTK=OFF                                                          \
-    -DWITH_GTK=OFF                                                          \
-    -DWITH_LAPACK=OFF                                                       \
-    -DWITH_1394=OFF                                                         \
-    -DWITH_JASPER=OFF                                                       \
-    -DWITH_OPENCLAMDFFT=OFF                                                 \
-    -DWITH_OPENCLAMDBLAS=OFF                                                \
-    -DWITH_VA=OFF                                                           \
-    -DWITH_VA_INTEL=OFF                                                     \
-    -DWITH_PROTOBUF=OFF                                                     \
-    -DWITH_TESSERACT=OFF                                                    \
     -DINSTALL_C_EXAMPLES=OFF                                                \
     -DENABLE_CONFIG_VERIFICATION=ON                                         \
     "${PYTHON_CMAKE_ARGS[@]}"                                               \
     -DINSTALL_PYTHON_EXAMPLES=ON                                            \
-    -DENABLE_PYLINT=1                                                       \
-    -DENABLE_FLAKE8=1                                                       \
+    -DENABLE_PYLINT=0      `# used for docs and examples`                   \
+    -DENABLE_FLAKE8=0                                                       \
     -DOPENCV_EXTRA_MODULES_PATH="../opencv_contrib-${PKG_VERSION}/modules"  \
     "${CMAKE_EXTRA_ARGS[@]}"                                                \
     "${CMAKE_DEBUG_ARGS[@]}"
