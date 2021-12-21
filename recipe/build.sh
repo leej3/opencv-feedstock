0c7e0a003e6fa449a150f10e8a362e5dc79eda4a
leej3 <johnleenimh@gmail.com>
2021-12-21 10:31:04 +0000

    more tidying

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,80 +14,99 @@ declare -a PYTHON_CMAKE_ARGS
 echo "PYTHON_CMAKE_ARGS="
 echo "${PYTHON_CMAKE_ARGS[@]}"
 
-# TODO: add if for architectures. ppc needs cmake_args to be set.
+# Set defaults for dependencies that change across OSes
+IFS=" " read -r WITH_PROTOBUF WITH_GSTREAMER WITH_QT <<< "0 0 0"
+
+# Capture cmake_args from build config yaml
+CBC_CMAKE_ARGS="${CMAKE_ARGS}"
+
+# Set some OS-specific vars
 declare -a CMAKE_EXTRA_ARGS
-export CXXFLAGS="$CXXFLAGS -D__STDC_CONSTANT_MACROS"
+# TODO: add if for architectures. ppc needs cmake_args to be set.
+if [[ ${target_platform} == osx-* ]]; then
+  CMAKE_EXTRA_ARGS+=("-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}")
+elif [[ ${target_platform} == x86_64 ]];then
+  WITH_QT=1
+  WITH_GSTREAMER=1
+  WITH_PROTOBUF=1
+elif [[ ${target_platform} == ppc64le ]];then
+    CMAKE_EXTRA_ARGS+=("${CMAKE_ARGS}")
+else
 # TODO: check if this is required
+export CXXFLAGS="$CXXFLAGS -D__STDC_CONSTANT_MACROS"
+
 export CPPFLAGS="${CPPFLAGS//-std=c++17/-std=c++11}"
 export CXXFLAGS="${CXXFLAGS//-std=c++17/-std=c++11}"
-
-if [[ ${target_platform} == osx-64 ]]; then
-  CMAKE_EXTRA_ARGS+=(-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT})
 fi
 
-mkdir build
-cd build
+# append dependencies to CMAKE_EXTRA_ARGS
+for dep in GSTREAMER PROTOBUF QT;do
+    varname=WITH_${dep}
+    CMAKE_EXTRA_ARGS+=("-D${varname}=${!varname}")
+done
+# append debug args
+CMAKE_EXTRA_ARGS+=("${CMAKE_DEBUG_ARGS[@]}")
+echo "CMake_EXTRA_ARGS : ${CMAKE_EXTRA_ARGS[@]}"
+
 #TODO: check that libpng is found or use conda forge hack
 
 #export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
 #export PKG_CONFIG_LIBDIR=$PREFIX/lib
-  cmake .. -LAH \
-    "${CMAKE_EXTRA_ARGS[@]}"        `# append above`                        \
-    "${PYTHON_CMAKE_ARGS[@]}"                                               \
-    -DCMAKE_BUILD_TYPE="Release"                                            \
-    -DCMAKE_CROSSCOMPILING=ON         `# may not need`                      \
-    -DENABLE_CONFIG_VERIFICATION=ON                                         \
-    -DENABLE_FLAKE8=0                                                       \
-    -DENABLE_PYLINT=0      `# used for docs and examples`                   \
-    -DINSTALL_C_EXAMPLES=OFF                                                \
-    -DINSTALL_PYTHON_EXAMPLES=ON                                            \
-    -DOPENCV_DOWNLOAD_PATH="${SYS_PREFIX}"/conda-bld/src_cache              \
-    -DOPENCV_EXTRA_MODULES_PATH="../opencv_contrib-${PKG_VERSION}/modules"  \
-    -DOpenCV_INSTALL_BINARIES_PREFIX=""                                     \
-    -DPYTHON_DEFAULT_EXECUTABLE=$(which python)                             \
-    -DPROTOBUF_UPDATE_FILES=ON                                              \
-    "${CMAKE_DEBUG_ARGS[@]}" `# append above`                               \
-    -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=ONLY                                \
-    -DCMAKE_FIND_ROOT_PATH="${PREFIX};${BUILD_PREFIX};${CONDA_BUILD_SYSROOT}"\
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}                                        \
-    -DCMAKE_INSTALL_REMOVE_ENVIRONMENT_RPATH=ON                             \
-    -GNinja                                                                 \
-    -DBUILD_DOCS=0                                                          \
-    -DBUILD_JASPER=0                                                        \
-    -DBUILD_JPEG=0                                                          \
-    -DBUILD_LIBPROTOBUF_FROM_SOURCES=0                                      \
-    -DBUILD_OPENEXR=ON                                                      \
-    -DBUILD_PERF_TESTS=0                                                    \
-    -DBUILD_PNG=0                                                           \
-    -DBUILD_PROTOBUF=0                                                      \
-    -DBUILD_TESTS=0                                                         \
-    -DBUILD_TIFF=0                                                          \
-    -DBUILD_ZLIB=0                                                          \
-    -DBUILD_opencv_apps=OFF `# issue linking with opencv_model_diagnostics` \
-    -DWITH_1394=OFF                                                         \
-    -DWITH_CUDA=OFF                                                         \
-    -DWITH_EIGEN=1                                                          \
-    -DWITH_FFMPEG=ON                                                        \
-    -DWITH_GTK=OFF                                                          \
-    -DWITH_ITT=OFF                                                          \
-    -DWITH_JASPER=OFF                                                       \
-    -DWITH_LAPACK=OFF                                                       \
-    -DWITH_MATLAB=OFF                                                       \
-    -DWITH_OPENCL=OFF                                                       \
-    -DWITH_OPENCLAMDBLAS=OFF                                                \
-    -DWITH_OPENCLAMDFFT=OFF                                                 \
-    -DWITH_OPENMP=1                                                         \
-    -DWITH_OPENNI=OFF                                                       \
-    -DWITH_QT=ON              `# only shoudl be  on for intel arch`         \
-    -DWITH_TESSERACT=OFF                                                    \
-    -DWITH_VA=OFF                                                           \
-    -DWITH_VA_INTEL=OFF                                                     \
-    -DWITH_VTK=OFF
+mkdir build
+cd build
+cmake .. -LAH GNinja                                                      \
+  "${CMAKE_EXTRA_ARGS[@]}" `#includes platform specific deps and options` \
+  "${PYTHON_CMAKE_ARGS[@]}"                                               \
+  -DBUILD_DOCS=0                                                          \
+  -DBUILD_JASPER=0                                                        \
+  -DBUILD_JPEG=0                                                          \
+  -DBUILD_LIBPROTOBUF_FROM_SOURCES=0                                      \
+  -DBUILD_OPENEXR=ON                                                      \
+  -DBUILD_PERF_TESTS=0                                                    \
+  -DBUILD_PNG=0                                                           \
+  -DBUILD_PROTOBUF=0                                                      \
+  -DBUILD_TESTS=0                                                         \
+  -DBUILD_TIFF=0                                                          \
+  -DBUILD_ZLIB=0                                                          \
+  -DBUILD_opencv_apps=OFF `# issue linking with opencv_model_diagnostics` \
+  -DCMAKE_BUILD_TYPE="Release"                                            \
+  -DCMAKE_CROSSCOMPILING=ON         `# may not need`                      \
+  -DCMAKE_FIND_ROOT_PATH="${PREFIX};${BUILD_PREFIX};${CONDA_BUILD_SYSROOT}"\
+  -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=ONLY                                \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX}                                        \
+  -DCMAKE_INSTALL_REMOVE_ENVIRONMENT_RPATH=ON                             \
+  -DENABLE_CONFIG_VERIFICATION=ON                                         \
+  -DENABLE_FLAKE8=0                                                       \
+  -DENABLE_PYLINT=0      `# used for docs and examples`                   \
+  -DINSTALL_C_EXAMPLES=OFF                                                \
+  -DINSTALL_PYTHON_EXAMPLES=ON                                            \
+  -DOPENCV_DOWNLOAD_PATH="${SYS_PREFIX}"/conda-bld/src_cache              \
+  -DOPENCV_EXTRA_MODULES_PATH="../opencv_contrib-${PKG_VERSION}/modules"  \
+  -DOpenCV_INSTALL_BINARIES_PREFIX=""                                     \
+  -DPROTOBUF_UPDATE_FILES=ON  `# should be used if using protobuf`        \
+  -DWITH_1394=OFF                                                         \
+  -DWITH_CUDA=OFF                                                         \
+  -DWITH_EIGEN=1                                                          \
+  -DWITH_FFMPEG=ON                                                        \
+  -DWITH_GTK=OFF                                                          \
+  -DWITH_ITT=OFF                                                          \
+  -DWITH_JASPER=OFF                                                       \
+  -DWITH_LAPACK=OFF                                                       \
+  -DWITH_MATLAB=OFF                                                       \
+  -DWITH_OPENCL=OFF                                                       \
+  -DWITH_OPENCLAMDBLAS=OFF                                                \
+  -DWITH_OPENCLAMDFFT=OFF                                                 \
+  -DWITH_OPENMP=1                                                         \
+  -DWITH_OPENNI=OFF                                                       \
+  -DWITH_TESSERACT=OFF                                                    \
+  -DWITH_VA=OFF                                                           \
+  -DWITH_VA_INTEL=OFF                                                     \
+  -DWITH_VTK=OFF
 
-  if [[ ! $? ]]; then
-    echo "configure failed with $?"
-    exit 1
-  fi
+if [[ ! $? ]]; then
+  echo "configure failed with $?"
+  exit 1
+fi
 
-  cmake --build . && cmake --install .
+cmake --build . && cmake --install .
 
